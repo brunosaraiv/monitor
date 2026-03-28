afb12cee2f9cc6e29edb49dbeb92469b8968b536
brunosaraiv <86279963+brunosaraiv@users.noreply.github.com>
2026-03-28 17:08:19 +0000

    [RESET] app minimo para subir railway

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "FUNCIONANDO 🚀"
+    return "OK HOME"
 
 @app.route("/health")
 def health():
-    return "OK"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    print(f"Rodando na porta {port}")
-    app.run(host="0.0.0.0", port=port)
+    return "OK HEALTH"
